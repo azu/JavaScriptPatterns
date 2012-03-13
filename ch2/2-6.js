4f41c73669e2390321708a8aeddf8b5e3bda4b08
azu <azuciao@gmail.com>
2012-03-13 23:07:32 +0900

    コーディングスタイルまで

---
+++ ch2/2-6.js
@@ -0,0 +1,16 @@
+/**
+ * Created by azu.
+ * Date: 12/03/13 22:04
+ * License: MIT License
+ */
+var inspect_me = 0, result = '';
+switch (inspect_me){
+    case 0:
+        result = "zero";
+        break;
+    case 0:
+        result = "one";
+        break;
+    default:
+        result = "unknown";
+}
---
+++ ch2/2-8.js
@@ -0,0 +1,11 @@
+/**
+ * Created by azu.
+ * Date: 12/03/13 22:28
+ * License: MIT License
+ */
+
+var month = "06";
+var year = "09";
+month = parseInt(month, 10);
+year = parseInt(year, 10);
+console.log(month, year);
