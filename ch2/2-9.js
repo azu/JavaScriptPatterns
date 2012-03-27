37f67f38e112ec4fb3a85a379068a697a0ff87c7
azu <azuciao@gmail.com>
2012-03-27 23:04:56 +0900

    2.14へ

---
+++ ch2/2-9.js
@@ -0,0 +1,15 @@
+/**
+ * Created by azu.
+ * Date: 12/03/27 22:06
+ * License: MIT License
+ */
+
+var d = 0,
+        a = b + 1;
+if (a && b && c){
+    d = a % c;
+    a += d;
+}
+(function(){
+
+})();
