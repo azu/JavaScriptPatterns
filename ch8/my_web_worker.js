f1c9614daffaeb047ce30df9abc1f650982911eb
azu <azuciao@gmail.com>
2013-08-27 22:23:20 +0900

    8.4.2 WebWorker

---
+++ ch8/my_web_worker.js
@@ -0,0 +1,15 @@
+/**
+ * Created by azu on 2013/08/27.
+ */
+
+var end = 1e8, tmp = 1;
+postMessage("Hello there");
+
+while (end) {
+    end -= 1;
+    tmp += end;
+    if (end == 5e7) {
+        postMessage("halfway there");
+    }
+}
+postMessage("all done");
